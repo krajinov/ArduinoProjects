c79a64aa732f324a5b004ca04e2967eff9e56a90
krajinov <krajinov.86@gmail.com>
2017-11-25 00:54:19 +0100

    RTC TG

--- Weekend Projects TG/RTC_ds1307_TEST/RTC_ds1307_TEST.ino
+++ Weekend Projects TG/RTC_ds1307_TEST/RTC_ds1307_TEST.ino
@@ -26,11 +26,11 @@ void setup () {
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
-    //    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    //        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
   } else {
     // January 21, 2014 at 3am you would call:
-//    rtc.adjust(DateTime(2017, 8, 19, 15, 14, 50));
+//    rtc.adjust(DateTime(2017, 9, 25, 20, 54, 20));
   }
 }
 
